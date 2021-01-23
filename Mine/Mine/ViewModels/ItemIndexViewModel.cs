6d6ef98fd8b84b6a080f549fbdd76c8fb8822b26
Hung Huynh <huynhh1@seattleu.edu>
2021-01-23 14:28:38 -0800

    Added delete Method to ItemIndexViewModel

--- Mine/Mine/ViewModels/ItemIndexViewModel.cs
+++ Mine/Mine/ViewModels/ItemIndexViewModel.cs
@@ -66,5 +66,28 @@ namespace Mine.ViewModels
 
             return result;
         }
+
+        /// <summary>
+        /// Delete the record from the system
+        /// </summary>
+        /// <param name="data">The Record to delte</param>
+        /// <returns>True if Deleted</returns>
+        public async Task<bool>DeleteAsync(ItemModel data)
+        {
+            //check if the record exists, if it does not, then null is returned
+            var record = await ReadAsync(data.Id);
+            if (record == null)
+            {
+                return false;   
+            }
+
+            //remove from the local data set cache
+            DataSet.Remove(data);
+
+            //Call to remove it form the Ddata Store
+            var result = await DataStore.DeleteAsync(data.Id);
+
+            return result;
+        }
     }
 }
