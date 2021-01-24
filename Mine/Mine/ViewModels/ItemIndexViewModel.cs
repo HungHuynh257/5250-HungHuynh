630bc2f0e298aa7ec210b6b0ff1d9001338f2b53
Hung Huynh <huynhh1@seattleu.edu>
2021-01-23 16:02:45 -0800

    Added Update Message and method to ItemIndexViewModel.cs, ItemUpdatePage.xaml.cs

--- Mine/Mine/ViewModels/ItemIndexViewModel.cs
+++ Mine/Mine/ViewModels/ItemIndexViewModel.cs
@@ -33,6 +33,12 @@ namespace Mine.ViewModels
                 var data = item as ItemModel;
                 await DeleteAsync(data);
             });
+
+            MessagingCenter.Subscribe<ItemUpdatePage, ItemModel>(this, "UpdateItem", async (obj, item) =>
+            {
+                var data = item as ItemModel;
+                await UpdateAsync(data);
+            });
         }
 
         async Task ExecuteLoadItemsCommand()
@@ -95,5 +101,30 @@ namespace Mine.ViewModels
 
             return result;
         }
+
+        /// <summary>
+        /// Update the record form the system
+        /// </summary>
+        /// <param name="data">The record to update</param>
+        /// <returns>True if Updated</returns>
+        public async Task<bool> UpdateAsync(ItemModel data)
+        {
+            //check if the record exists, if it does not, then null is returned
+            var record = await ReadAsync(data.Id);
+            if (record == null)
+            {
+                return false;
+            }
+
+            //call to remove it from the data store
+            var result = await DataStore.UpdateAsync(data);
+
+            var canExecute = LoadItemsCommand.CanExecute(null);
+            LoadItemsCommand.Execute(null);
+
+            return result;
+        }
+
+
     }
 }
