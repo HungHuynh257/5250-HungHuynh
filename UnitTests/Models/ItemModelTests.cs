86cd05460db725a089f4f0c5acce87340857a253
Hung Huynh <huynhh1@seattleu.edu>
2021-02-08 22:26:26 -0800

    Added ItemModel_Get UT to ItemModelTests.cs

--- UnitTests/Models/ItemModelTests.cs
+++ UnitTests/Models/ItemModelTests.cs
@@ -21,5 +21,37 @@ namespace UnitTests.Models
             Assert.IsNotNull(result);
         }
 
+        [Test]
+        public void ItemModel_Set_Get_Valid_Default_should_PAss()
+        {
+            //Arrange
+
+            //Act
+            var result = new ItemModel();
+            result.Description = "Description";
+            result.Id = "ID";
+            result.Text = "Text";
+            result.Value = 1;
+
+            //reset
+            //Assert
+            Assert.AreEqual("Description", result.Description);
+            Assert.AreEqual("ID", result.Id);
+            Assert.AreEqual("Text", result.Text);
+            Assert.AreEqual(1, result.Value);
+        }
+
+        [Test]
+        public void ItemModel_Get_Valid_Default_Should_Pass()
+        {
+            //Arrange
+            //Act
+            var result = new ItemModel();
+
+            //Rest
+
+            //Assert
+            Assert.AreEqual(0, result.Value);
+        }
     }
 }
