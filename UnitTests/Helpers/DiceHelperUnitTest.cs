a57a30ee9d1e1a22c6e6cefc3edcef45d125d0a9
Hung Huynh <huynhh1@seattleu.edu>
2021-02-08 23:11:01 -0800

    Add RollDice valid roll 1 dice 6 to DiceHelperUnitTests.cs

--- UnitTests/Helpers/DiceHelperUnitTest.cs
+++ UnitTests/Helpers/DiceHelperUnitTest.cs
@@ -24,6 +24,26 @@ namespace UnitTests.Helpers
                 // Assert 
                 Assert.AreEqual(0, result);
             }
+
+            [Test]
+            public void RollDice_Valid_Roll_1_Dice_6_Should_Return_Between_1_And_6()
+            {
+                // Arrange
+                DiceHelper.ForceRollsToNotRandom = true;
+                DiceHelper.ForcedRandomValue = 1;
+
+                // Act
+                var result = DiceHelper.RollDice(1, 6);
+
+                // Reset
+                DiceHelper.ForceRollsToNotRandom = false;
+
+                // Assert 
+                Assert.AreEqual(true, result >= 1);
+                Assert.AreEqual(true, result <= 6);
+            }
+
+            
         }
     }
 }
